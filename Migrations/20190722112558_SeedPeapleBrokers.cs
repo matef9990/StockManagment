5585d6a738c95fae1d9ee78bed327ab1694fdc03
Mohamed Atef <m.atef9990@gmail.com>
2019-07-22 15:14:29 +0200

    seed Brokers and People  In DataBase and add Credintial to Cors

---
+++ Migrations/20190722112558_SeedPeapleBrokers.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StockManagment.Migrations
+{
+    public partial class SeedPeapleBrokers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Brokers (Name) VALUES ('FxPro')");
+            migrationBuilder.Sql("INSERT INTO PEOPLE(Name,BrokerId) Values ('Mahamed Atef',(Select BrokerId From Brokers Where Name='FxPro'))");
+            migrationBuilder.Sql("INSERT INTO PEOPLE(Name,BrokerId) Values ('Mazen Mohamed',(Select BrokerId From Brokers Where Name='FxPro'))");
+
+        }
+
+
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM People WHERE Name In('Mahamed Atef','Mazen Mohamed')");
+            migrationBuilder.Sql("DELETE FROM Brokers WHERE Name In('FxPro')");
+        }
+    }
+}
